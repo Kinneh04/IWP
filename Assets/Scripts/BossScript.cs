ea9a9050f99aced5775df1d620874cbabf0a3c48
LilKinny <kohlitegd@gmail.com>
2023-11-07 00:09:28 +0800

    boss dash movement

--- Assets/Scripts/BossScript.cs
+++ Assets/Scripts/BossScript.cs
@@ -8,9 +8,17 @@ public class BossScript : MonoBehaviour
 
     public Transform player;
 
-    private void Start()
+    public float dashSpeed = 5.0f;
+    public float minDistance = 3.0f;
+    public float maxDistance = 10.0f;
+
+    private Vector3 targetPosition;
+    private bool isDashing = false;
+
+    void Start()
     {
-        player = GameObject.FindGameObjectWithTag("PlayerHitbox").transform;
+        // Call Dash method when you want the boss to dash
+        // Example: DashToRandomLocation();
     }
 
     void Update()
@@ -19,5 +27,34 @@ public class BossScript : MonoBehaviour
         {
             transform.LookAt(player);
         }
+        if (isDashing)
+        {
+            float step = dashSpeed * Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, step);
+
+            // Check if we've reached the target
+            if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
+            {
+                isDashing = false;
+            }
+        }
+    }
+
+    public void DashToLocation(Vector3 destination)
+    {
+        targetPosition = destination;
+        isDashing = true;
+    }
+
+    public void DashToRandomLocation()
+    {
+        Vector3 randomDirection = Random.onUnitSphere;
+        randomDirection.y = 0; // Keep it on the same plane as the boss
+        randomDirection.Normalize();
+
+        float randomDistance = Random.Range(minDistance, maxDistance);
+        Vector3 destination = transform.position + randomDirection * randomDistance;
+
+        DashToLocation(destination);
     }
 }
