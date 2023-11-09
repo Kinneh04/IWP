a3fe5a6354eb1a232cd584ae9a98b83bf4db4b1d
LilKinny <kohlitegd@gmail.com>
2023-11-09 21:42:05 +0800

    Partial integration of playfab V1

--- Assets/Scripts/OfficialSongManager.cs
+++ Assets/Scripts/OfficialSongManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Unity.VisualScripting;
-
+using PlayFab;
+using PlayFab.ClientModels;
+using Newtonsoft.Json;
 public class OfficialSongManager : MonoBehaviour
 {
     [Header("Fill in")]
@@ -13,20 +15,75 @@ public class OfficialSongManager : MonoBehaviour
     public EnemySpawner enemyManager;
     public Button PlayButton;
     public TMP_Text SongSelectText;
+    public List<OfficialSongScript> OfficialSongsList = new List<OfficialSongScript>();
+
+    [Header("Popupmenu")]
+    public GameObject SelectedPopupMenu;
+    public TMP_Text SongNameText, SongTimeText, SongBPMText, SongDifficultyText, SongBossText;
+    public GameObject LeaderboardScoreprefab;
+    public Transform LeaderboardPrefabParent;
 
     [Header("Dont fill in")]
     public OfficialSongScript CurrentlySelectedSong;
+    void GetOfficialSongScores()
+    {
+        PlayFabClientAPI.GetTitleData(new GetTitleDataRequest
+        {
+            Keys = new List<string> { "OfficialSongScores" }
+        }, OnGetTitleDataSuccess, OnGetTitleDataFailure);
+    }
+
+    void OnGetTitleDataSuccess(GetTitleDataResult result)
+    {
+        if (result.Data.TryGetValue("OfficialSongScores", out string officialSongScores))
+        {
+            // You have successfully retrieved the data. Use 'officialSongScores' for further processing.
+        }
+        else
+        {
+            Debug.Log("OfficialSongScores not found in title data.");
+        }
+    }
+    void SerializeAndStoreData()
+    {
+        string serializedData = JsonConvert.SerializeObject(OfficialSongsList);
+
+        Dictionary<string, string> titleData = new Dictionary<string, string>
+        {
+            { "OfficialSongData", serializedData }
+        };
+
+        PlayFabClientAPI.SetTitleData(new SetTitleDataRequest
+        {
+            Data = titleData
+        }, OnSetTitleDataSuccess, OnSetTitleDataFailure);
+    }
+
+    void OnSetTitleDataSuccess(SetTitleDataResult result)
+    {
+        Debug.Log("Data successfully stored in title data.");
+    }
+
+    void OnSetTitleDataFailure(PlayFabError error)
+    {
+        Debug.LogError("Error storing data in title data: " + error.GenerateErrorReport());
+    }
+    void OnGetTitleDataFailure(PlayFabError error)
+    {
+        Debug.LogError("Error retrieving title data: " + error.GenerateErrorReport());
+    }
 
     private void Start()
     {
         PlayButton.interactable = false;
+        GetOfficialSongScores();
     }
     public void LoadSong()
     {
         musicController.BPM_Divider = CurrentlySelectedSong.BPM;
         musicController.MusicAudioSource.clip = CurrentlySelectedSong.SongAudioClip;
         musicController.LightColorPalette.Clear();
-        foreach(Color c in CurrentlySelectedSong.colors)
+        foreach (Color c in CurrentlySelectedSong.colors)
         {
             musicController.LightColorPalette.Add(c);
         }
@@ -36,7 +93,7 @@ public class OfficialSongManager : MonoBehaviour
 
     public void PreviewSong(OfficialSongScript SS)
     {
-     
+
         if (CurrentlySelectedSong != SS)
         {
             StopAllCoroutines();
@@ -44,12 +101,30 @@ public class OfficialSongManager : MonoBehaviour
             StartCoroutine(TransitionToNewSong());
             PlayButton.interactable = true;
             SongSelectText.text = "Selected: " + SS.TitleOfSong;
+
+            SelectedPopupMenu.SetActive(true);
+            SongNameText.text = SS.TitleOfSong;
+            int length = (int)SS.SongAudioClip.length;
+            int minutes = length / 60;
+            int seconds = length % 60;
+
+            string formattedTime = string.Format("{0}:{1:00}", minutes, seconds);
+
+            SongTimeText.text = "TIME: " + formattedTime;
+            SongBPMText.text = "BPM: " + SS.BPM.ToString();
+            SongDifficultyText.text = "DIFFICULTY: " + SS.DifficultyOverride.ToString() + "/10";
+            if (SS.ContainsBoss)
+            {
+                SongBossText.text = "CONTAINS BOSS";
+            }
+            else SongBossText.text = "";
+
         }
     }
 
     public IEnumerator TransitionToNewSong()
     {
-        while(MainMenuAudioSource.volume > 0)
+        while (MainMenuAudioSource.volume > 0)
         {
             MainMenuAudioSource.volume -= Time.deltaTime;
             yield return null;
@@ -57,10 +132,11 @@ public class OfficialSongManager : MonoBehaviour
         MainMenuAudioSource.clip = CurrentlySelectedSong.SongAudioClip;
         MainMenuAudioSource.time = CurrentlySelectedSong.PreviewStartTime;
         MainMenuAudioSource.Play();
-        while(MainMenuAudioSource.volume < 1)
+        while (MainMenuAudioSource.volume < 1)
         {
             MainMenuAudioSource.volume += Time.deltaTime;
             yield return null;
         }
     }
 }
+
--- Assets/Scripts/OfficialSongScript.cs
+++ Assets/Scripts/OfficialSongScript.cs
@@ -10,10 +10,25 @@ public class OfficialSongScript : MonoBehaviour
     public int BPM;
     public List<SongEvent> Events = new List<SongEvent>();
     public int DifficultyOverride;
+    public bool ContainsBoss;
     
     [Header("optional")]
     public float PreviewStartTime;
 
+    [Header("Leaderboard")]
+    public List<LeaderboardEntry> leaderboardEntries = new List<LeaderboardEntry>();
+
     [Header("For Debugging only!")]
     public float OfficialStartTime;
 }
+
+
+[System.Serializable]
+public class LeaderboardEntry
+{
+    public string LBName;
+    public string LBAccuracy;
+    public string LBRanking;
+    public string LBScore;
+}
+
